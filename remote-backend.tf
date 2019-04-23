33f266b60d75baae3019e621a31cb82959b27193
John Hubert <31088992+JPHubert@users.noreply.github.com>
2019-04-23 16:40:54 -0500

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "azurerm" {
-        resource_group_name     = "secdevops"
-        storage_account_name    = "secdevopssa"
+        resource_group_name     = "msdn-tfstate-rg"
+        storage_account_name    = "msdn-tfstatesa2524"
         container_name          = "terraformstate"
-        key                     = "msdn.terraform.tfstate"
+        key                     = "test.terraform.tfstate"
         access_key              = ""
     }
 }
